19ba84afa20848e381c904dc5bcfc52c8ae3e666
jpabdou <johnpabdou@gmail.com>
2022-12-30 02:26:40 -0800

    all projects and resources tasks completed

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,4 +1,25 @@
 // build your `Resource` model here
 const db = require("../../data/dbConfig")
 
-module.exports = null
+const get = async (id) => {
+    let rows
+    if (id) {
+        rows = await db("resources").where("resource_id", id).first()
+    } else {
+        rows = await db("resources")
+    }
+    return rows
+}
+
+const locate = async (name) =>{
+    const row = await db("resources").where("resource_name", name).first()
+    return row
+}
+
+const insert = async (resource) => {
+    const id = await db("resources").insert(resource)
+    const row = await get(id)
+    return row
+}
+
+module.exports = {get, locate,insert}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,41 @@
 // build your `/api/resources` router here
 const express = require("express")
 const resourcesRouter = express.Router()
+const Resources = require("./model")
+
+resourcesRouter.get("/", async (req,res, next)=>{
+    const results = await Resources.get()
+    try {
+        res.status(200).json(results)
+    }
+    catch(error) {
+        next(error)
+    }
+})
+
+resourcesRouter.post("/", async (req, res, next)=> {
+    const {resource_name} = req.body
+    if (resource_name && typeof resource_name === "string" && resource_name.trim().length){
+        const match = await Resources.locate(resource_name)
+        if (match) {
+            next({status: 400, message: "name taken"})
+        } else {
+            const resource = await Resources.insert(req.body)
+            try {
+                res.status(200).json(resource)
+            } 
+            catch(error) {
+                next(error)
+            }
+        }
+    } else {
+        next({status: 400, message: "name required"})
+    }
+})
+
+resourcesRouter.use((error, req, res, next)=>{
+    res.status(error.status || 500).json({message: error.message || "bad request"})
+})
 
 
 
